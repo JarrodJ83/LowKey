6dc664537e76231684dc88c8c9f1b3034c7cf74a
Jarrod Johnson <jarrod.johnson@alsoenergy.com>
2021-10-30 10:26:45 -0400

    Test connection string is for correct datasource

--- LowKey.Data.UnitTests/DbConnectionFactoryTests.cs
+++ LowKey.Data.UnitTests/DbConnectionFactoryTests.cs
@@ -1,6 +1,7 @@
 ﻿using AutoFixture.Xunit2;
 using LowKey.Data.Sql;
 using System.Data.Common;
+using System.Data.SqlClient;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -18,8 +19,24 @@ namespace LowKey.Data.UnitTests
         public async Task CreateDbConnection(Db testDb)
         {
             var conn = await _dbConnFactory.CreateForStore(testDb);
-            
+
             Assert.NotNull(conn);
+        }
+
+        [Theory, AutoData]
+        public async Task ConnectionIsForCorrectDatabase(Db testDb)
+        {
+            var conn = await _dbConnFactory.CreateForStore(testDb);
+
+            Assert.Equal(conn.Database, testDb.Name);
+            var connectionStringBuilder = new SqlConnectionStringBuilder(conn.ConnectionString);
+
+            var datasourceParts = connectionStringBuilder.DataSource.Split(":");
+            var server = datasourceParts[0];
+            var port = datasourceParts[1];
+
+            Assert.Equal(server, testDb.Server);
+            Assert.Equal(int.Parse(port), testDb.Port);
         }
     }
 }
