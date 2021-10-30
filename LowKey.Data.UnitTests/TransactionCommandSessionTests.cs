e071542d8d918d87660ab5ee4ad5117b73da5f1e
Jarrod Johnson <jarrod.johnson@alsoenergy.com>
2021-10-30 15:32:17 -0400

    Add TransactionalCommandSession and tests

---
+++ LowKey.Data.UnitTests/TransactionCommandSessionTests.cs
@@ -0,0 +1,82 @@
+﻿using AutoFixture.Xunit2;
+using LowKey.Data.Transactions;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Transactions;
+using Xunit;
+
+namespace LowKey.Data.UnitTests
+{
+    public class TransactionCommandSessionTests
+    {
+        ICommandSession<TestClient> _commandSession;
+        public TransactionCommandSessionTests()
+        {
+            _commandSession = new TransactionalCommandSession<TestClient>(new TestCommandSession(), TransactionScopeOption.Required, new TransactionOptions
+            {
+                IsolationLevel = IsolationLevel.ReadCommitted
+            });
+        }
+
+        [Fact]
+        public async Task TransactionIsPresentWithinCommandHandler()
+        {
+            await _commandSession.Execute(new Db("test", "", 0), client =>
+            {
+                Assert.NotNull(Transaction.Current);
+                return Task.CompletedTask;
+            });
+        }
+
+        [Fact]
+        public Task TransactionHasCorrectSettings()
+        {
+            var transactionScopeOptions = TransactionScopeOption.Required;
+            var transactionOptions = new TransactionOptions
+            {
+                IsolationLevel = IsolationLevel.ReadCommitted,
+                Timeout = TimeSpan.MaxValue
+            };
+
+            _commandSession = new TransactionalCommandSession<TestClient>(new TestCommandSession(), transactionScopeOptions, transactionOptions);
+
+            return _commandSession.Execute(new Db("test", "", 0), client =>
+            {
+                Assert.Equal(transactionOptions.IsolationLevel, Transaction.Current?.IsolationLevel);
+                return Task.CompletedTask;
+            });
+        }
+
+        [Fact]
+        public async Task TransactionIsCommitted()
+        {
+            var transactionScopeOptions = TransactionScopeOption.Required;
+            var transactionOptions = new TransactionOptions
+            {
+                IsolationLevel = IsolationLevel.ReadCommitted,
+                Timeout = TimeSpan.MaxValue
+            };
+
+            _commandSession = new TransactionalCommandSession<TestClient>(new TestCommandSession(), transactionScopeOptions, transactionOptions);
+
+            Transaction transaction;
+                        
+            await _commandSession.Execute(new Db("test", "", 0), client =>
+            {
+                transaction = Transaction.Current;
+                return Task.CompletedTask;
+            });
+
+            Assert.Equal(TransactionStatus.Committed, TransactionStatus.Committed);
+        }
+    }
+
+    class TestCommandSession : ICommandSession<TestClient>
+    {
+        public Task Execute(Db db, Func<TestClient, Task> command, CancellationToken cancellation = default)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ LowKey.Data/Transactions/TransactionalCommandSession.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace LowKey.Data.Transactions
+{
+    public class TransactionalCommandSession<TClient> : ICommandSession<TClient>
+    {
+        private readonly ICommandSession<TClient> _commandSession;
+        private readonly TransactionScopeOption _transactionScopeOption;
+        private readonly TransactionOptions _transactionOptions;
+
+        public TransactionalCommandSession(ICommandSession<TClient> commandSession, TransactionScopeOption transactionScopeOption, TransactionOptions transactionOptions)
+        {
+            _commandSession = commandSession;
+            _transactionOptions = transactionOptions;
+            _transactionScopeOption = transactionScopeOption;
+        }
+
+        public async Task Execute(Db db, Func<TClient, Task> command, CancellationToken cancellation = default)
+        {
+            using var trxScope = new TransactionScope(_transactionScopeOption, _transactionOptions, TransactionScopeAsyncFlowOption.Enabled);
+            await _commandSession.Execute(db, command, cancellation);
+        }
+    }
+}
