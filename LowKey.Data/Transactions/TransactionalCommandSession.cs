4d9e87f770230eb75305acbff4355768743b0327
Jarrod Johnson <jarrod.johnson@alsoenergy.com>
2021-11-07 13:57:55 -0500

    Add TransactionSettings class to encapsulate settings

--- LowKey.Data/Transactions/TransactionalCommandSession.cs
+++ LowKey.Data/Transactions/TransactionalCommandSession.cs
@@ -5,22 +5,32 @@ using System.Transactions;
 
 namespace LowKey.Data.Transactions
 {
+    public class TransactionSettings
+    {
+        public TransactionScopeOption TransactionScopeOption;
+        public TransactionOptions TransactionOptions;
+
+        public TransactionSettings(TransactionScopeOption transactionScopeOption, TransactionOptions transactionOptions)
+        {
+            TransactionScopeOption = transactionScopeOption;
+            TransactionOptions = transactionOptions;
+        }        
+    }
+
     public class TransactionalCommandSession<TClient> : ICommandSession<TClient>
     {
+        private readonly TransactionSettings _settings;
         private readonly ICommandSession<TClient> _commandSession;
-        private readonly TransactionScopeOption _transactionScopeOption;
-        private readonly TransactionOptions _transactionOptions;
-
-        public TransactionalCommandSession(ICommandSession<TClient> commandSession, TransactionScopeOption transactionScopeOption, TransactionOptions transactionOptions)
+        
+        public TransactionalCommandSession(ICommandSession<TClient> commandSession, TransactionSettings settings)
         {
+            _settings = settings;
             _commandSession = commandSession;
-            _transactionOptions = transactionOptions;
-            _transactionScopeOption = transactionScopeOption;
         }
 
         public async Task Execute(Db db, Func<TClient, Task> command, CancellationToken cancellation = default)
         {
-            using var trxScope = new TransactionScope(_transactionScopeOption, _transactionOptions, TransactionScopeAsyncFlowOption.Enabled);
+            using var trxScope = new TransactionScope(_settings.TransactionScopeOption, _settings.TransactionOptions, TransactionScopeAsyncFlowOption.Enabled);
             await _commandSession.Execute(db, command, cancellation);
             trxScope.Complete();
         }
