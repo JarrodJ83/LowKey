8ef50fc4bb444a21d7da3328efa8c3bdf1b6ded4
Jarrod Johnson <jarrod.johnson.accts@protonmail.com>
2022-01-16 17:38:25 -0500

    Add unit testing project

---
+++ LowKey.Data.UnitTests/LowKeyDataHostingExtensionsTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace LowKey.Data.UnitTests
+{
+    public class LowKeyDataHostingExtensionsTests
+    {
+        
+        public LowKeyDataHostingExtensionsTests()
+        {
+        }
+
+        [Fact]
+        public async Task Register_Single_Db_With_Single_Tenant_With_Single_Client()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public async Task Register_Multiple_Dbs_With_Single_Tenant_With_Single_Client()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public async Task Register_Single_Db_With_Multiple_Tenant()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public async Task Register_Single_Db_With_Single_Tenant()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
