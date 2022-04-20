0d72b56182d0fd495e80256ca2ef6c92b0390fa2
Jarrod Johnson <jarrod.johnson.accts@protonmail.com>
2022-04-19 21:12:37 -0400

    Support DbConnection and SqlConnection

--- LowKey.Data.Sql/SqlDbConnectionFactory.cs
+++ LowKey.Data.Sql/SqlDbConnectionFactory.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace LowKey.Data.Sql
 {
-    public class SqlDbConnectionFactory : IClientFactory<DbConnection>
+    public class SqlDbConnectionFactory : IClientFactory<DbConnection>, IClientFactory<SqlConnection>
     {
         private readonly SqlConnectionStringBuilder _sqlConnectionStringBuilder;
 
@@ -14,18 +14,31 @@ namespace LowKey.Data.Sql
             _sqlConnectionStringBuilder = sqlConnectionStringBuilder;
         }
 
-        public Task<DbConnection> CreateForStore(DataStore dataStore, Tenant tenant)
+        Task<DbConnection> IClientFactory<DbConnection>.CreateForStore(DataStore dataStore, Tenant tenant)
         {
-            _sqlConnectionStringBuilder.InitialCatalog = dataStore.Name;
-            _sqlConnectionStringBuilder.DataSource = tenant.Server;
-                
-            if(tenant.Port.HasValue)
-            {
-                _sqlConnectionStringBuilder.DataSource += $":{tenant.Port}";
-            }
+            ConfigureConnectionStringBuilder(dataStore, tenant);
 
             DbConnection connection = new SqlConnection(_sqlConnectionStringBuilder.ConnectionString);
             return Task.FromResult(connection);
+        }
+
+        Task<SqlConnection> IClientFactory<SqlConnection>.CreateForStore(DataStore dataStore, Tenant tenant)
+        {
+            ConfigureConnectionStringBuilder(dataStore, tenant);
+
+            var connection = new SqlConnection(_sqlConnectionStringBuilder.ConnectionString);
+            return Task.FromResult(connection);
+        }
+
+        private void ConfigureConnectionStringBuilder(DataStore dataStore, Tenant tenant)
+        {
+            _sqlConnectionStringBuilder.InitialCatalog = dataStore.Name;
+            _sqlConnectionStringBuilder.DataSource = tenant.Server;
+
+            if (tenant.Port.HasValue)
+            {
+                _sqlConnectionStringBuilder.DataSource += $":{tenant.Port}";
+            }
         }
     }
 }
