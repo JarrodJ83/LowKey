f591695031afa6943fab82a771a770eb538206fd
Jarrod Johnson <jarrod.johnson@alsoenergy.com>
2022-05-05 09:52:20 -0400

    Remove Diagnostics. Unnecessary for this library

--- LowKey.Data/MultiTenancy/Diagnostics/LowKeyDataActivityTags.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace LowKey.Data.MultiTenancy.Diagnostics
-{
-    public static class LowKeyDataActivityTags
-    {
-        public const string ClientType = "client.type";
-        public const string QueryResultType = "query.result.type";
-    }
-}
