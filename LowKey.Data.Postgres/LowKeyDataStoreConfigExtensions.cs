e5be0be524b4d588070b9bcba4bc262ea3cba049
Jarrod Johnson <jarrod.johnson@alsoenergy.com>
2022-05-05 10:07:21 -0400

    Add Postgres support

---
+++ LowKey.Data.Postgres/LowKeyDataStoreConfigExtensions.cs
@@ -0,0 +1,19 @@
+﻿using LowKey.Data;
+using LowKey.Data.Postgres;
+using LowKey.Data.Extensions.Hosting;
+using Npgsql;
+using System.Data.Common;
+
+namespace Microsoft.Extensions.Hosting
+{
+    public static class NpsqlDataStoreConfigExtensions
+    {
+        public static LowKeyDataStoreConfig WithSqlServer(this LowKeyDataStoreConfig config, NpgsqlConnectionStringBuilder connectionStringBuilder)
+        {
+            config.UseClientFactory(() => (IClientFactory<DbConnection>)new NpgsqlDbConnectionFactory(connectionStringBuilder));
+            config.UseClientFactory(() => (IClientFactory<NpgsqlConnection>)new NpgsqlDbConnectionFactory(connectionStringBuilder));
+
+            return config;
+        }
+    }
+}
---
+++ LowKey.Data.Postgres/NpgsqlDbConnectionFactory.cs
@@ -0,0 +1,43 @@
+﻿using LowKey.Data.Model;
+using Npgsql;
+using System.Data.Common;
+
+namespace LowKey.Data.Postgres
+{
+    public class NpgsqlDbConnectionFactory : IClientFactory<DbConnection>, IClientFactory<NpgsqlConnection>
+    {
+        private readonly NpgsqlConnectionStringBuilder _connStrBuilder;
+
+        public NpgsqlDbConnectionFactory(NpgsqlConnectionStringBuilder connStrBuilder)
+        {
+            _connStrBuilder = connStrBuilder;
+        }
+
+        Task<DbConnection> IClientFactory<DbConnection>.CreateForStore(DataStore dataStore, Tenant tenant, CancellationToken cancel)
+        {
+            ConfigureConnectionStringBuilder(dataStore, tenant);
+
+            DbConnection connection = new NpgsqlConnection(_connStrBuilder.ConnectionString);
+            return Task.FromResult(connection);
+        }
+
+        Task<NpgsqlConnection> IClientFactory<NpgsqlConnection>.CreateForStore(DataStore dataStore, Tenant tenant, CancellationToken cancel)
+        {
+            ConfigureConnectionStringBuilder(dataStore, tenant);
+
+            var connection = new NpgsqlConnection(_connStrBuilder.ConnectionString);
+            return Task.FromResult(connection);
+        }
+
+        private void ConfigureConnectionStringBuilder(DataStore dataStore, Tenant tenant)
+        {
+            _connStrBuilder.Host = tenant.Server;
+            _connStrBuilder.Database = dataStore.Name;
+
+            if (tenant.Port.HasValue)
+            {
+                _connStrBuilder.Port = tenant.Port.Value;
+            }
+        }
+    }
+}
