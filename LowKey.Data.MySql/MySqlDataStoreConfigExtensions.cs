c3bc50088aaf5a02d52532110262eeca019cc10d
Jarrod Johnson <jarrod.johnson.accts@protonmail.com>
2022-10-02 14:13:19 -0400

    Add MySql Support

---
+++ LowKey.Data.MySql/MySqlDataStoreConfigExtensions.cs
@@ -0,0 +1,21 @@
+﻿using LowKey.Data;
+using LowKey.Data.Sql;
+using LowKey.Data.Extensions.Hosting;
+using System.Data.Common;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+namespace Microsoft.Extensions.Hosting
+{
+    public static class MySqlDataStoreConfigExtensions
+    {
+        public static LowKeyDataStoreConfig WithMySql(this LowKeyDataStoreConfig config, MySqlConnectionStringBuilder connectionStringBuilder)
+        {
+            config.UseClientFactory(() => (IClientFactory<IDbConnection>)new MySqlDbConnectionFactory(connectionStringBuilder));
+            config.UseClientFactory(() => (IClientFactory<DbConnection>)new MySqlDbConnectionFactory(connectionStringBuilder));
+            config.UseClientFactory(() => (IClientFactory<MySqlConnection>)new MySqlDbConnectionFactory(connectionStringBuilder));
+
+            return config;
+        }
+    }
+}
---
+++ LowKey.Data.MySql/MySqlDbConnectionFactory.cs
@@ -0,0 +1,43 @@
+﻿using LowKey.Data.Model;
+using MySql.Data.MySqlClient;
+using System.Data.Common;
+
+namespace LowKey.Data.Sql
+{
+    public class MySqlDbConnectionFactory : IClientFactory<DbConnection>, IClientFactory<MySqlConnection>
+    {
+        private readonly MySqlBaseConnectionStringBuilder _mySqlConnectionStringBuilder;
+
+        public MySqlDbConnectionFactory(MySqlBaseConnectionStringBuilder mySqlConnectionStringBuilder)
+        {
+            _mySqlConnectionStringBuilder = mySqlConnectionStringBuilder;
+        }
+
+        Task<DbConnection> IClientFactory<DbConnection>.CreateForStore(DataStore dataStore, Tenant tenant, CancellationToken cancel)
+        {
+            ConfigureConnectionStringBuilder(dataStore, tenant);
+
+            DbConnection connection = new MySqlConnection(_mySqlConnectionStringBuilder.ConnectionString);
+            return Task.FromResult(connection);
+        }
+
+        Task<MySqlConnection> IClientFactory<MySqlConnection>.CreateForStore(DataStore dataStore, Tenant tenant, CancellationToken cancel)
+        {
+            ConfigureConnectionStringBuilder(dataStore, tenant);
+
+            var connection = new MySqlConnection(_mySqlConnectionStringBuilder.ConnectionString);
+            return Task.FromResult(connection);
+        }
+
+        private void ConfigureConnectionStringBuilder(DataStore dataStore, Tenant tenant)
+        {
+            _mySqlConnectionStringBuilder.Database = dataStore.Name;
+            _mySqlConnectionStringBuilder.Server = tenant.Server;
+
+            if (tenant.Port.HasValue)
+            {
+                _mySqlConnectionStringBuilder.Port = (uint)tenant.Port.Value;
+            }
+        }
+    }
+}
