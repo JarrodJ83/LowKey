e5697f94fcf7c5c7d8f81300ada7d625c6654e7c
Jarrod Johnson <jarrod.johnson.accts@protonmail.com>
2022-10-02 21:41:31 -0400

    Add claim based TenantId resolver

---
+++ LowKey.Data.MultiTenancy.ClaimBased/ClaimBasedTenantIdResolver.cs
@@ -0,0 +1,41 @@
+﻿using LowKey.Data.Model;
+using Microsoft.AspNetCore.Http;
+using System.Security.Claims;
+
+namespace LowKey.Data.MultiTenancy.Jwt
+{
+    public class ClaimBasedTenantIdResolver : ITenantIdResolver
+    {
+        public class Settings
+        {
+            public string TenantIdClaimName { get; }
+
+            public Settings(string tenantIdClaimName)
+            {
+                TenantIdClaimName = tenantIdClaimName;
+            }
+        }
+
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly Settings _settings;
+
+        public ClaimBasedTenantIdResolver(IHttpContextAccessor httpContextAccessor, Settings settings)
+        {
+            _settings = settings;
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public Task<TenantId> Resolve()
+        {
+            var user = _httpContextAccessor.HttpContext?.User;
+
+            if (user == null) throw new InvalidOperationException("No user could be found to resolve TenantId from.");
+
+            Claim? tenantIdClaim = user.Claims.SingleOrDefault(claim => claim.Type.Equals(_settings.TenantIdClaimName));
+
+            if (tenantIdClaim == null) throw new InvalidOperationException($"No claim with type \"{_settings.TenantIdClaimName}\" could be found");
+
+            return Task.FromResult(new TenantId(tenantIdClaim.Value));
+        }
+    }
+}
